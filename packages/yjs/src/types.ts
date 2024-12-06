f9a216cf546f898ac172496b610ed995e9d181b2
Chandler Dykes <cjd0067@auburn.edu>
2024-12-06 15:49:50 -0600

    Seem to have gotten the type to stop breaking editor

--- packages/yjs/src/types.ts
+++ packages/yjs/src/types.ts
@@ -1,3 +1,4 @@
+import { JsonObject, JsonValue } from "type-fest";
 import * as Y from "yjs";
 
 export type SupportedYType = Y.Array<any> | Y.Map<any> | Y.Text;
@@ -5,21 +6,35 @@ export type SupportedYType = Y.Array<any> | Y.Map<any> | Y.Text;
 type MaxDepth = 25;
 
 export type StringPropertyPath<
-  T,
+  T extends JsonObject,
   P extends string = "",
   D extends number[] = []
 > = D["length"] extends MaxDepth
   ? never
   : T extends string
   ? P
-  : T extends (infer U)[]
-  ? StringPropertyPath<U, `${P}[]`, [0, ...D]>
-  : T extends object
-  ? {
-      [K in keyof T & (string | number)]: StringPropertyPath<
-        T[K],
-        P extends "" ? `${K}` : `${P}.${K}`,
-        [0, ...D]
-      >;
-    }[keyof T & (string | number)]
-  : never;
+  : {
+      [K in keyof T & string]: T[K] extends JsonObject
+        ? StringPropertyPath<
+            T[K],
+            P extends "" ? `${K}` : `${P}.${K}`,
+            [0, ...D]
+          >
+        : T[K] extends (infer U)[]
+        ? U extends JsonObject
+          ? StringPropertyPath<
+              U,
+              P extends "" ? `${K}[]` : `${P}.${K}[]`,
+              [0, ...D]
+            >
+          : U extends string
+          ? P extends ""
+            ? `${K}[]`
+            : `${P}.${K}[]`
+          : never
+        : T[K] extends string
+        ? P extends ""
+          ? `${K}`
+          : `${P}.${K}`
+        : never;
+    }[keyof T & string];
